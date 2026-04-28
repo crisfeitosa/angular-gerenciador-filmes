b1ca5acf0c61a686d9268d47d5b7b43629eb2aff
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-04-28 15:20:41 -0300

    feat: enhance MoviesFilter and ExploreMovies components with improved data handling and structure

--- src/app/features/movies/components/movies-filter/movies-filter.ts
+++ src/app/features/movies/components/movies-filter/movies-filter.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MoviesListResponse } from '../../../../shared/types/movies-list-response';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movies-filter',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './movies-filter.html',
-  styleUrl: './movies-filter.css',
+  styleUrls: ['./movies-filter.css'],
 })
 export class MoviesFilter {
-
+  movies = input<MoviesListResponse>([]);
 }
